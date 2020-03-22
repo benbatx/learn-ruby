c88edb4d5e4cad825dfb3a6e5616fd1ff526a641
root <root@GERTRUDE.localdomain>
2020-03-22 17:48:18 -0500

    I finished up to problem 9!

--- problems/4_rock_paper_scissors.rb
+++ problems/4_rock_paper_scissors.rb
@@ -1,7 +1,23 @@
-move = gets.strip
+move = gets().strip() #functions - always give back a string
 # output the move that wins the rock paper scissors game
 # rock -> paper
 # paper -> scissors
 # scissors -> rock
-
-
+if false #independent if statements
+  if move == "rock"
+    puts "paper"
+  end
+  if move == "paper"
+    puts "scissors"
+  end
+  if move == "scissors"
+    puts "rock"
+  end
+end
+if move == "rock" #dependent
+  puts "paper"
+elsif move == "paper"
+  puts "scissors"
+elsif move == "scissors"
+  puts "rock"
+end
--- problems/9_count_vowels.rb
+++ problems/9_count_vowels.rb
@@ -3,5 +3,11 @@ word = gets.strip
 # elday -> 2
 # ben -> 1
 # covidsucks -> 3
-
-
+#loop, +=, count
+letters = word.split('')
+vowels = "aeiou".split('')
+total_vowels = 0
+vowels.each do |vowel|
+  total_vowels += letters.count(vowel)
+end
+puts(total_vowels)
