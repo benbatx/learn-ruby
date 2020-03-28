01de969f085460d522f359972543a120c21ac413
benbatx <benbatx@gmail.com>
2020-03-28 01:55:38 -0500

    a

---
+++ specs/count_words/should_pass/6_count_words.rb
@@ -0,0 +1,8 @@
+sentence = gets.strip
+# output the number of words in sentence
+
+# yes please do pleaded alice -> 5
+# chapter iv -> 2
+# are you to get in at all said the footman -> 10
+# but at any rate he might answer questions -> 8
+puts sentence.split(' ').length
